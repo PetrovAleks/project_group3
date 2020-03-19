1ce4ce7a88cbe968cf977f75f4616b7519907d57
Polina Slyusareva <ghileors@gmail.com>
2020-03-19 18:15:48 +0200

    add hero section button at bottom

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,4 +4,4 @@ document.querySelector('.menu-icon-wrapper').onclick = function() {
 
 document.querySelector('.menu-icon-wrapper').onclick = function() {
   document.querySelector('.nav__list').classList.toggle('nav__list--popup');
-}
+};

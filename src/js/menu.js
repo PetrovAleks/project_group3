f68392dfe6966c59152582bc9379b1d1fb180d09
Polina Slyusareva <ghileors@gmail.com>
2020-03-20 13:15:58 +0200

    fix bag with  menu-active

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,4 @@
 document.querySelector('.menu-icon-wrapper').onclick = function() {
   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-};
-
-document.querySelector('.menu-icon-wrapper').onclick = function() {
   document.querySelector('.nav__list').classList.toggle('nav__list--popup');
 };

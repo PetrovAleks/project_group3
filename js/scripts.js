a5f90a4c134ffca2a5c1b1ac3e5d2d2f3d19ef7d
Petrov Aleksandr <rpm10b@gmail.com>
2020-03-20 13:33:10 +0200

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+"use strict";document.querySelector(".menu-icon-wrapper").onclick=function(){document.querySelector(".menu-icon").classList.toggle("menu-icon-active"),document.querySelector(".nav__list").classList.toggle("nav__list--popup")};
